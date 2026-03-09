5fadbfc653e4d7a5a2a162412a0815271571bc10
AutomizePlusSoft <automizeplus001@gmail.com>
2026-03-09 17:05:32 +0530

    ok

--- FreightBKShippingWebApp/Program.cs
+++ FreightBKShippingWebApp/Program.cs
@@ -157,16 +157,16 @@ builder.Services.Configure<ForwardedHeadersOptions>(options =>
 // ==========================================================
 
 // 🔵 Local API
-builder.Services.AddHttpClient<ApiClient>(client =>
-{
-    client.BaseAddress = new Uri("https://localhost:5003/");
-});
-
-// 🔴 Production API (Uncomment in production)
 //builder.Services.AddHttpClient<ApiClient>(client =>
 //{
-//    client.BaseAddress = new Uri("https://apihost.freightbook.in/");
+//    client.BaseAddress = new Uri("https://localhost:5003/");
 //});
+
+// 🔴 Production API (Uncomment in production)
+builder.Services.AddHttpClient<ApiClient>(client =>
+{
+    client.BaseAddress = new Uri("https://apihost.freightbook.in/");
+});
 
 // ==========================================================
 // 🔟 LOCALIZATION
