221320f01e77b03d00bbd06743c3795d5fffab08
prashant kharat <prashant00095@gmail.com>
2026-03-21 18:09:44 +0530

    prashant changes

---
+++ FreightBKShippingWebApp/Report/ReceiptReport1.cs
@@ -0,0 +1,16 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace FreightBKShippingWebApp.Report
+{
+    public partial class ReceiptReport1 : DevExpress.XtraReports.UI.XtraReport
+    {
+        public ReceiptReport1()
+        {
+            InitializeComponent();
+        }
+    }
+}
